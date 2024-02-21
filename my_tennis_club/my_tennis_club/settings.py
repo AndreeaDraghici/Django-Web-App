df1084da246ce5df1995613a6be52e76e3f9cc21
AndreeaDraghici <draghici108@gmail.com>
2024-02-21 22:02:03 +0200

    added main index page

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5pe=i2)1@hadl7jlrp3ny040j=jfa5#(vs5w)6woy8cfoyjn)-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
